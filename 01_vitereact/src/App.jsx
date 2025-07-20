5e3f2b53c4da7cbd609e83a1f78a8d7dd0f901a1
amaan <anonymousdarkzone1@gmail.com>
2025-07-20 16:35:57 +0530

    1

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+ 
+
+  return (
+  <h1>hello with</h1>
+  )
+}
+
+export default App
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>chai aur react| amaan bhai </h1>
+  );
+}
+
+export default App;
